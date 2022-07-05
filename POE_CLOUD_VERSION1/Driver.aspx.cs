29cb93eab4c9dff3982ec83a6c996435f7de5c99
Siyabonga Zunguza <siyabonga4767@gmail.com>
2022-07-05 21:01:17 +0200

    done

--- POE_CLOUD_VERSION1/Driver.aspx.cs
+++ POE_CLOUD_VERSION1/Driver.aspx.cs
@@ -11,7 +11,7 @@ namespace POE_CLOUD_VERSION1
 {
     public partial class Driver : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(@"Data Source=siyabongasv.database.windows.net;Initial Catalog=RideYouRent_St10091114;User ID=st10091114;Password=Drugkind22");
+        SqlConnection con = new SqlConnection(@"Data Source=tcp:siyabongasv.database.windows.net,1433;Initial Catalog=RideYouRent_St10091114;User Id=st10091114@siyabongasv;Password=Drugkind22");
         SqlCommand cmd;
         DataTable dt;
         SqlDataAdapter da;
